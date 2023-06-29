94b4f72ed436010179697c5deca71802e57a5742
Mahammad Hajiyev <MahammadHajiyev1@gmail.com>
2023-06-29 20:34:52 +0400

    redeploy

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import "./Main.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "../components/userList";
 import UserPosts from "../components/userPosts";
 import PageNotFound from "../components/pageNotFound";
